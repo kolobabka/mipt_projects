8c4dab8c4a2f4606056cb413927822a3c528050b
kolobaka <belov.vv@phystech.edu>
2020-11-22 04:14:04 +0300

    update

---
+++ Onegin/Onegin.c
@@ -0,0 +1,216 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <sys/stat.h>
+#include <ctype.h>
+#include <string.h>
+
+//-----------------------------------------------------------------------------
+
+int char_cmp_up (const void *first, const void *second );
+int char_cmp_low (const void *first, const void *second );
+void str_sep (char* buffer, size_t size, char** strings, size_t num_str);
+int count_strings (char* get_text, size_t size);
+void file_read_func (char* buffer, size_t size, char* const text_name);
+int size_of_text (char* const text_name);
+
+//-----------------------------------------------------------------------------
+
+int main()
+{
+  char** strings = NULL;
+  char* buffer = NULL;
+  size_t size = 0, num_str = 0;
+  char* const text_name = "text.md";
+
+  size = size_of_text(text_name);
+  assert (size);
+
+  buffer = (char*) calloc(size, sizeof(char));
+  assert (buffer);
+
+  file_read_func (buffer, size, text_name);
+  num_str = count_strings(buffer, size);
+  assert (num_str);
+
+  strings = (char**) calloc (num_str, sizeof(char*));
+  assert (strings);
+
+  str_sep (buffer, size, strings, num_str);
+
+  qsort (strings, num_str, sizeof (char*), char_cmp_low);
+
+  for (int prnt_count = 0; prnt_count < num_str; prnt_count++)
+    printf("%s\n", strings[prnt_count]);
+
+  printf("\n");
+  qsort (strings, num_str, sizeof (char*), char_cmp_up);
+
+  printf("\n\n\nINVEEEEEEEEEEEEEEERSE\n\n\n" );
+
+  for (int prnt_count = 0; prnt_count < num_str; prnt_count++)
+    printf("%s\n", strings[prnt_count]);
+
+  free(buffer);
+  free(strings);
+
+  return 0;
+}
+
+//-----------------------------------------------------------------------------
+
+void file_read_func (char* buffer, size_t size, char* const text_name)
+{
+  assert (buffer);
+  assert (text_name);
+  assert (size);
+
+  FILE* text = fopen(text_name, "r");
+  assert(text);
+
+  int reader = fread (buffer, sizeof(char), size, text);
+  ferror(text);
+
+  fclose(text);
+}
+
+//-----------------------------------------------------------------------------
+
+int count_strings (char* buffer, size_t size)
+{
+  assert (buffer);
+  assert (size);
+
+  size_t count = 0, flag = 0;
+
+  for (size_t num_count = 0; num_count < size; num_count++)
+  {
+    if (num_count == size - 1 && (buffer[num_count] == '\n') && flag == 0)
+      break;
+    if ( (buffer[num_count] == '\n' || buffer[num_count] == '\t' || buffer[num_count] == ' ' || isdigit (buffer[num_count]) || ispunct (buffer[num_count])) && flag == 0)
+    {
+     continue;
+    }
+    if (buffer[num_count] == '\n')
+    {
+      count++;
+      flag = 0;
+      continue;
+    }
+    flag ++;
+  }
+
+  return count;
+}
+
+//-----------------------------------------------------------------------------
+
+void str_sep (char* buffer, size_t size, char** strings, size_t num_str)
+{
+  assert (strings);
+  assert (buffer);
+  assert (size);
+  assert (num_str);
+
+  size_t buf_count = 0, move_ptr = 0, flag_2 = 0;
+
+  for (size_t num_count = 0; num_count < num_str; num_count++)
+  {
+    move_ptr = 0;
+    for (buf_count; buf_count < size; buf_count++)
+    {
+      if ((buffer[buf_count] == '\n' || buffer[buf_count] == '\t' || buffer[buf_count] == ' ' || isdigit (buffer[buf_count]) || ispunct (buffer[buf_count]))  && flag_2 == 0)
+      {
+        buf_count += 1;
+        num_count -= 1;
+        break;
+      }
+      if (buffer[buf_count] == '\n')
+      {
+        buffer[buf_count] = '\0';
+        strings[num_count] = &(buffer[buf_count]) - move_ptr;
+        buf_count += 1;
+        flag_2 = 0;
+        break;
+      }
+      flag_2 ++;
+      move_ptr ++;
+    }
+  }
+}
+
+//-----------------------------------------------------------------------------
+
+int size_of_text (char* const text_name)
+{
+  assert (text_name);
+
+  struct stat size;
+  stat (text_name, &size);
+
+  return size.st_size;
+}
+
+//-----------------------------------------------------------------------------
+
+int char_cmp_low (const void *first, const void *second )
+{
+  assert (first);
+  assert (second);
+
+  char **str1 = (char**) first;
+  char **str2 = (char**) second;
+
+  size_t char_count_1 = 0;
+  size_t char_count_2 = 0;
+
+  while (str1[0][char_count_1] != '\0' && str2[0][char_count_2] != '\0')
+  {
+    while (ispunct (str1[0][char_count_1]) || isdigit (str1[0][char_count_1]) || isspace (str1[0][char_count_1]))
+      char_count_1++;
+
+    while (ispunct (str2[0][char_count_2]) || isdigit (str2[0][char_count_2]) || isspace (str2[0][char_count_2]))
+      char_count_2++;
+
+    while (tolower(str1[0][char_count_1]) == tolower(str2[0][char_count_2]) )
+    {
+      char_count_1++;
+      char_count_2++;
+    }
+    break;
+  }
+
+  return (tolower(str1[0][char_count_1]) - tolower(str2[0][char_count_2]));
+}
+
+//-----------------------------------------------------------------------------
+
+int char_cmp_up (const void *first, const void *second )
+{
+  assert (first);
+  assert (second);
+
+  char **str1 = (char**) first;
+  char **str2 = (char**) second;
+
+  size_t char_count_1 = strlen (str1[0]) - 1;
+  size_t char_count_2 = strlen (str2[0]) - 1;
+
+  while (char_count_1 >= 0 && char_count_2 >= 0)
+  {
+    while (ispunct (str1[0][char_count_1]) || isdigit (str1[0][char_count_1]) || isspace (str1[0][char_count_1]))
+      char_count_1--;
+
+    while (ispunct (str2[0][char_count_2]) || isdigit (str2[0][char_count_2]) || isspace (str2[0][char_count_2]))
+      char_count_2--;
+
+    while (tolower(str1[0][char_count_1]) == tolower(str2[0][char_count_2]) )
+    {
+      char_count_1--;
+      char_count_2--;
+    }
+    break;
+  }
+
+  return (tolower(str1[0][char_count_1]) - tolower(str2[0][char_count_2]));
+}
---
+++ QuadraticEq/QuadraticEqDed.c
@@ -0,0 +1,267 @@
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#include "UnTst.h"
+
+#define NO_ROOTS  0
+#define ONE_ROOT  1
+#define TWO_ROOTS  2
+#define INF_ROOTS  3
+
+//..........................................................
+void Quadeq_Test();
+int choice();
+void Lineq_Test();
+void UNIT_TESTS();
+int SolvQuad(double a, double b, double c, double* x1, double* x2);
+int linequation(double b, double c, double* x1);
+int is_zero(double* e);
+
+//..........................................................
+
+int main() {
+
+	double a = 0, b = 0, c = 0;
+
+	printf("\n"
+		"SolvQuadratic Equation, v 1.0.0\n"
+		"Enter coeffs a, b, c for equation ax^2 + bx + c = 0. Enter a:");
+
+
+	int check = scanf("%lf", &a);
+	assert( check == 1);
+
+	printf("                                                     Enter b:");
+
+	check = scanf("%lf", &b);
+	assert( check == 1);
+
+	printf("                                                     Enter c:");
+
+	check = scanf("%lf", &c);
+	assert( check == 1);
+
+	double x1 = 0, x2 = 0;
+	if (a == 0) {
+		int check_func_1 = linequation(b, c, &x1);
+
+		switch (check_func_1)
+			{
+			case NO_ROOTS:  printf ("No roots!\n");                 break;
+
+			case ONE_ROOT:  printf ("Only one root, %lf\n", x1);    break;
+
+			case INF_ROOTS: printf ("Infinite number of roots!\n"); break;
+			}
+	}
+	else {
+		int check_func_2 = SolvQuad(a, b, c, &x1, &x2);
+
+		switch (check_func_2)
+		{
+			case NO_ROOTS: printf("No roots!");                     break;
+
+			case ONE_ROOT: printf("Only one root, %lf\n", x1);      break;
+
+			case TWO_ROOTS: printf("Two roots:\n"
+				"The first root is x1 = %lf\n"
+				"The second root is x2 = %lf\n", x1, x2);             break;
+
+			case INF_ROOTS: printf("Infinite number of roots!\n");  break;
+		}
+	}
+
+	printf("\n\nThat's all! We've just finished to solve this equation!\n\n");
+
+	UNIT_TESTS();
+	choice();
+	return 0;
+}
+
+//..........................................................
+
+int SolvQuad(double a, double b, double c, double* x1, double* x2)
+{
+
+
+	if (a != 0) {
+		double D = b * b - 4 * a * c;
+
+
+		if (D == 0) {
+			*x1 = -b / (2 * a);
+			 int ger = is_zero(x1);
+
+			return ONE_ROOT;
+		}
+		else
+
+		if (D > 0) {
+			double sqrt_d = sqrt(D);
+
+			*x1 = (-b + sqrt_d) / (2 * a);
+			*x2 = (-b - sqrt_d) / (2 * a);
+			int ger = is_zero(x1);
+			is_zero(x2);
+
+			return TWO_ROOTS;
+
+		}
+
+		if (D < 0)
+
+			return NO_ROOTS;
+	}
+
+}
+
+//..........................................................
+
+int linequation(double b, double c, double *x1)
+{
+
+		if (b != 0) {
+			*x1 = -c / b;
+			int ger = is_zero(x1);
+
+			return ONE_ROOT;
+		}
+
+		if ((b == 0) && (c != 0))
+		{
+
+			return NO_ROOTS;
+
+		}
+
+		if ((b == 0) && (c == 0)) {
+
+			return INF_ROOTS;
+		}
+
+}
+
+//..........................................................
+
+int is_zero(double* e )
+{
+		if (fabs(*e) <= 10e-6) *e = 0;
+}
+
+//..........................................................
+
+int choice()
+{
+	char c;
+	c = '0';
+	while ((c = getchar()) != '\n');
+	c = getchar();
+
+	if ( c == 'Y' || c == 'y')
+	{
+		Lineq_Test();
+		Quadeq_Test();
+		printf("\n\t\t\t<<<<We've finished to test our, thank you>>>>\n\n");
+		return 0;
+	}
+
+	if ( c == 'N'|| c == 'n')
+	{
+		printf("Ok,Goodbye,my friend!!!\n\n");
+		return 0;
+	}
+
+	printf("Sorry, I'm a stupid robot and I don't understand this command :(\n"
+					"Try again!\n\n");
+	choice();
+}
+
+//..........................................................
+
+/*void UNIT_TESTS()
+{
+	printf("Do you want to check correctness of working your program? Please, enter 'Y' if you want or 'N' if not\n\n");
+}*/
+
+//..........................................................
+
+/*void Lineq_Test()
+{
+	int n = 0;
+	double x = 0;
+	printf("\n\t\t\t<<<<Let's check the function 'linequation' solving linear equations (line 119)>>>>\n\n");
+
+	int lin = linequation(3,4, &x);
+	double expect = -4.0 / 3.0;
+	if (x == expect && lin == ONE_ROOT) { printf("The first test is OK\n");
+										 n++;
+									 }
+	else printf("\tWell, something gets wrong and the first test failed\n"
+							"An expected result is %lf (return %d);the function has returned the other value(return %d),"
+							"or has some troubles with arithmetic (x = %lf).\n"
+							"Please try to check the lines 121-123,"
+							"maybe we have mistackes in the code.\n\n", expect, ONE_ROOT, lin, x);
+
+	lin = linequation(0,4, &x);
+	if (lin == NO_ROOTS) { printf("The first test is OK\n");
+										 		 n++;
+											 }
+	else printf("\n\tIt's a pity! The second test failed\n"
+							"The result must be 'No Roots!', but something gets wrong.\n"
+							"Please try to check the lines 129-132, maybe we have mistackes in the code.\n\n");
+
+  lin = linequation(0,0, &x);
+	if (lin == INF_ROOTS) { printf("The first test is OK\n");
+										      n++;
+									 		  }
+	else printf("\n\tOoohps, the third test failed\n"
+							"The result must be 'Infinity number of roots!', but something gets wrong.\n"
+							"Please try to check the lines 136-138, maybe we have mistackes in the code.\n\n");
+
+	if (n == 3) printf("\nCongratulations! All tests are done!\n");
+
+}*/
+
+//..........................................................
+
+/*void Quadeq_Test()
+{
+	int n = 0;
+	double x1 = 0, x2 = 0;
+	printf("\n\t\t\t<<<<Checking the function 'SolvQuad' solving quadratic equations (line 83)>>>>\n\n");
+
+	int quad = SolvQuad(1,2,1, &x1, &x2);
+	double expect = -1.0;
+	if (x1 == expect && quad == ONE_ROOT) { printf("The first test is OK\n");
+										                     n++;
+									 										  }
+	else printf("\n\tThe first test failed\n"
+						  "An expected result is %lf (return %d);the function has returned the other value(return %d),"
+							"or has troubles with arithmetic (x = %lf).\n"
+							"Please try to check the lines 90-94,"
+							"maybe we have mistackes in the code.\n\n", expect, ONE_ROOT, quad, x1);
+
+	quad = SolvQuad(1,3,-4, &x1, &x2);
+	double expect1 = -4.0;
+	double expect2 = 1.0;
+	if ((x1 == expect1 || x1 == expect2) && (x2 == expect2 || x2 == expect1) && quad == TWO_ROOTS)
+																				 { printf("The second test is OK\n");
+										                      n++;
+									 										   }
+	else printf("\n\tPanic! The second test failed\n"
+						  "An expected result is %lf and %lf (return %d);the function has returned the other value(return %d),"
+							"or has troubles with arithmetic (x1 = %lf and x2 = %lf).\n"
+							"Please try to check the lines 96-106,"
+					  	"maybe we have mistackes in the code.\n\n", expect1, expect2, TWO_ROOTS, quad, x1, x2);
+
+	quad = SolvQuad(1,2,10, &x1, &x2);
+	if (quad == NO_ROOTS)
+												{ printf("The second test is OK\n");
+												  n++;
+												}
+ else printf("\n\tThe third test failed\n"
+             "The result must be 'No Roots!', but something gets wrong.\n"
+						 "Please try to check the lines 110-112, maybe we have mistackes in the code.\n\n");
+
+ if (n == 3) printf("\nWow-wow! All tests are done!\n");
+}*/
